45a81a10741b96fc356ba5fe05fb7d9319a88664
zeeshanhanif <zeeshanhanif@gmail.com>
2025-03-10 08:15:21 +0500

    Added arguments.js file for contract verification

---
+++ scripts/arguments.js
@@ -0,0 +1,15 @@
+module.exports = [
+    // "0x77319B4031e6eF1250907aa00018B8B1c67a244b",
+    // "0x04fbd8a2e56dd85CFD5500A4A4DfA955B9f1dE6f",
+    // "0x2E896b0b2Fdb7457499B56AAaA4AE55BCB4Cd316",
+    // "0xBe54FB168b3c982b7AaE60dB6CF75Bd8447b390E",
+    // "0x1514000000000000000000000000000000000000"
+
+    // "0x429b697b0Bc1491F298C997140B62760ad2B0E17", 
+    // "Adam",
+    // "Adam",
+    // "ADAM", 
+    // "https://green-vicarious-wildcat-394.mypinata.cloud/ipfs/bafkreih52un25x5afl4mbxp3nxpnglatrjdzyoufg7szugwfmi66zf5lau"
+
+    "0x429b697b0Bc1491F298C997140B62760ad2B0E17",
+];
